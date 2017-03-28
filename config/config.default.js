7bae9141a91a660d865b3e690e97a7bae5cf8756
dongxu <dongxu@dongxus-MacBook-Pro.local>
2017-03-28 16:31:41 +0800

    init

---
+++ config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1490689789804_8576';
+
+  return config;
+};
